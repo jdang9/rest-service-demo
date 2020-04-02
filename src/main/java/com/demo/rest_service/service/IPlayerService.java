f9c5e8cddf556a53d08128034cd8005b14c5e5f3
JDang <atd.dang@gmail.com>
2020-04-02 14:25:19 -0400

    add @QueryParam, @FormParam, @Context, and Cookies

--- src/main/java/com/demo/rest_service/service/IPlayerService.java
+++ src/main/java/com/demo/rest_service/service/IPlayerService.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 import com.demo.rest_service.model.Player;
 
-@Consumes("application/xml,application/json")
+@Consumes("application/xml,application/json,application/x-www-form-urlencoded")
 @Produces("application/xml,application/json")
 @Path("/playerservice")
 public interface IPlayerService {
@@ -25,11 +30,17 @@ public interface IPlayerService {
 	
 	@Path("/players/{id}")
 	@GET
-	Player getPlayer(@PathParam(value = "id") Long id);
+	Player getPlayerId(@PathParam(value = "id") Long id);
+	
+	@Path("/playersByNationality")
+	@GET
+	List<Player> getPlayersByNationality(@QueryParam("nation") String nation);
 	
 	@Path("/players")
 	@POST
-	Response createPlayer(Player player);
+	void createPlayer(@FormParam("name") String name, @FormParam("position") String position, 
+			@FormParam("nationality") String nationality, @HeaderParam("agent") String agent,
+			@Context HttpHeaders headers);
 	
 	@Path("/players")
 	@PUT
--- src/main/java/com/demo/rest_service/service/PlayerServiceImpl.java
+++ src/main/java/com/demo/rest_service/service/PlayerServiceImpl.java
@@ -3,9 +3,14 @@ package com.demo.rest_service.service;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Service;
@@ -37,21 +42,44 @@ public class PlayerServiceImpl implements IPlayerService {
 		Collection<Player> results = players.values();
 		ArrayList<Player> response = new ArrayList<Player>(results);
 
-		System.out.print("---------------" + response);
 		return response;
 	}
 
 	@Override
-	public Player getPlayer(Long id) {
+	public Player getPlayerId(Long id) {
 		return players.get(id);
 	}
 
 	@Override
-	public Response createPlayer(Player player) {
-		player.setId(++currId);
-		players.put(player.getId(), player);
+	public void createPlayer(String name, String position, String nationality, String agent, HttpHeaders headers) {
+		long id = ++currId;
+		Player newPlayer = new Player();
+		newPlayer.setId(id);
+		newPlayer.setName(name);
+		newPlayer.setPosition(position);
+		newPlayer.setNationality(nationality);
+		players.put(id, newPlayer);
 		
-		return Response.ok(player).build();
+		System.out.println("-----Agent-----");
+		System.out.println(agent); //this is to verify header being passed and print in console
+		
+		System.out.println("-----Headers-----");
+		//this is to print out all header props in console
+		MultivaluedMap<String, String> allHeaders = headers.getRequestHeaders();
+		Set<String> headerKeys = allHeaders.keySet();
+		
+		for (String key : headerKeys) {
+			System.out.println(headers.getHeaderString(key));
+		}
+		
+		System.out.println("-----Cookies-----");
+		//this is to print out cookies on console
+		Map<String, Cookie> cookies = headers.getCookies();
+		Set<String> cookieKeys = cookies.keySet();
+		
+		for (String key : cookieKeys) {
+			System.out.println(cookies.get(key).getValue());
+		}
 	}
 
 	@Override
@@ -59,7 +87,7 @@ public class PlayerServiceImpl implements IPlayerService {
 		Player currentPlayer = players.get(player.getId());
 		if (currentPlayer != null)
 		{
-			players.put(currentPlayer.getId(),currentPlayer);
+			players.put(currentPlayer.getId(), player);
 			
 			return Response.ok(currentPlayer).build();
 		}
@@ -78,6 +106,21 @@ public class PlayerServiceImpl implements IPlayerService {
 
 		return Response.notModified().build();	
 		
+	}
+
+	@Override
+	public List<Player> getPlayersByNationality(String nation) {
+		List<Player> response = new ArrayList<Player>();
+		Iterator<Player> results = players.values().iterator();
+		
+		while (results.hasNext()) {
+			Player player = results.next();
+			if (player.getNationality().equals(nation)) {
+				response.add(player);
+			}
+		}
+		
+		return response;
 	}
 
 }
